b4b0db60c0c406b9637f95364b83096245b72d7a
JoaoV <joao.vfm18@gmail.com>
2022-05-02 23:07:55 -0400

    fix: bug styled component

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-:root{
+  :root {
     --background: #f0f2f5;
     --red: #e52e4d;
     --blue: #5429cc;
@@ -10,61 +10,62 @@ export const GlobalStyle = createGlobalStyle`
     --text-title: #363f5f;
     --text-body: #969cb3;
     --shape: #ffffff;
-}
+  }
 
-*{ 
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-}
-html {
+  }
+
+  html {
     @media (max-width:1080px) {
-        font-size: 93.75%; // 15px
+      font-size: 93.75%; // 15px
     }
 
     @media (max-width: 720px) {
-        font-size: 87.5%; // 14px
+      font-size: 87.5%; // 14px
     }
-}
-body {
+  }
+
+  body {
     background: var(--background);
     -webkit-font-smoothing: antialiased;
-}
+  }
 
-body, input, textarea, button {
-font-family: 'Poppins', sans-serif;
-font-weight: 400;
-}
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
 
-h1 , h2 , h3 , h4 , h6 , strong {
+  h1 , h2 , h3 , h4 , h6 , strong {
     font-weight: 600;
-}
+  }
 
-button {
+  button {
     cursor: pointer;
-}
+  }
 
-[disabled] {
+  [disabled] {
     opacity: 0.6;
     cursor: not-allowed; // não permitdo, um curso diferente
-}
+  }
 
-.react-modal-overlay {
-  background: rgba(0, 0, 0, 0.5);
-  
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  left: 0;
-  
-  display: flex ;
-  align-items: center ;
-  justify-content: center;
-}
-.react-modal-content {
-  width: 100%;
-  max-width: 576px;
-}
+  .react-modal-overlay {
+    background: rgba(0, 0, 0, 0.5);
 
-`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex ;
+    align-items: center ;
+    justify-content: center;
+  }
+  .react-modal-content {
+    width: 100%;
+    max-width: 576px;
+  }
+`;
