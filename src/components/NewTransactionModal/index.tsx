395acd6dad7b0139fa9e53e88f56dff4c4b00e95
JoaoV <joao.vfm18@gmail.com>
2022-05-03 21:16:34 -0400

    feat: added styles in modal component

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,10 @@
 import Modal from 'react-modal';
-import {Container} from "./styles";
+import {Container, TransactionsTypeContainer} from "./styles";
+import closeImg from '../../assets/btnfechar.svg'
+import outImg from '../../assets/saida.svg'
+import incomeImg from '../../assets/entrada.svg'
+
+
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -14,11 +19,26 @@ export const NewTransactionModal = ({isOpen, onRequestClose}: NewTransactionModa
              overlayClassName='react-modal-overlay'
              className='react-modal-content'
       >
+          <button type='button' onClick={onRequestClose} className='react-modal-close'>
+              <img src={closeImg} alt="Fechar Modal"/>
+          </button>
           <Container>
               <h2>Cadastrar transação</h2>
 
               <input placeholder='Titulo'/>
               <input placeholder='Valor' type='number'/>
+
+              <TransactionsTypeContainer>
+                  <button type='button'>
+                      <img src={incomeImg} alt="Entrada"/>
+                      <span>Entrada</span>
+                  </button>
+                  <button type='button'>
+                      <img src={outImg} alt="Saída"/>
+                      <span>Saida</span>
+                  </button>
+              </TransactionsTypeContainer>
+
               <input placeholder='Categoria'/>
 
               <button type='submit'> Cadastrar </button>
